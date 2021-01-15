f3cacc2d8b2e8ee041610c09074e5b470ef84ef1
maksimpa <maksim.pasnitsenko@metropolia.fi>
2021-01-15 13:28:05 +0200

    ajax task C ready

--- components/List.js
+++ components/List.js
@@ -1,36 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {FlatList} from 'react-native';
+import {useLoadMedia} from '../hooks/ApiHooks';
 import ListItem from './ListItem';
 
-const baseUrl = `http://media.mw.metropolia.fi/wbma/`;
-
 const List = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-
-  const loadMedia = async (limit = 5) => {
-    try {
-      const listResponse = await fetch(baseUrl + 'media?limit=' + limit);
-      const listJson = await listResponse.json();
-      console.log('response json data', listJson);
-      const media = await Promise.all(
-        listJson.map(async (item) => {
-          const fileResponse = await fetch(baseUrl + 'media/' + item.file_id);
-          const fileJson = fileResponse.json();
-          // console.log('media file data', json);
-          return fileJson;
-        })
-      );
-      console.log('media array data', media);
-
-      setMediaArray(media);
-    } catch (error) {
-      console.error('loadMedia error', error);
-    }
-  };
-
-  useEffect(() => {
-    loadMedia(10);
-  }, []);
+  const mediaArray = useLoadMedia();
 
   return (
     <FlatList
