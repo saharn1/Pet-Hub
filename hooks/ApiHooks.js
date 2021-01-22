bdcfee136b3d6990f6f2af729cd97e53271e50e3
maksimpa <maksim.pasnitsenko@metropolia.fi>
2021-01-22 10:33:24 +0200

    login and register forms

--- hooks/ApiHooks.js
+++ hooks/ApiHooks.js
@@ -30,4 +30,47 @@ const useLoadMedia = () => {
   return mediaArray;
 };
 
-export {useLoadMedia};
+const useLogin = () => {
+  const postLogin = async (userCredentials) => {
+    const options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(userCredentials),
+    };
+    try {
+      const response = await fetch(baseUrl + 'login', options);
+      const userData = await response.json();
+      console.log('postLogin response status', response.status);
+      console.log('postLogin userData', userData);
+      if (response.ok) {
+        return userData;
+      } else {
+        throw new Error(userData.message);
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  const checkToken = async (token) => {
+    try {
+      const options = {
+        method: 'GET',
+        headers: {'x-access-token': token},
+      };
+      const response = await fetch(baseUrl + 'users/user', options);
+      const userData = response.json();
+      if (response.ok) {
+        return userData;
+      } else {
+        throw new Error(userData.message);
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  return {postLogin, checkToken};
+};
+
+export {useLoadMedia, useLogin};
--- views/Login.js
+++ views/Login.js
@@ -3,29 +3,47 @@ import {StyleSheet, View, Text, Button} from 'react-native';
 import PropTypes from 'prop-types';
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useLogin} from '../hooks/ApiHooks';
 
 const Login = ({navigation}) => {
   const [isLoggedIn, setIsLoggedIn] = useContext(MainContext);
-  console.log('ili', isLoggedIn);
+  console.log('isLoggedIn?', isLoggedIn);
+  const {postLogin, checkToken} = useLogin();
+
   const logIn = async () => {
-    setIsLoggedIn(true);
-    await AsyncStorage.setItem('userToken', 'abc');
-    if (isLoggedIn) {
-      // this is to make sure isLoggedIn has changed, will be removed later
-      navigation.navigate('Home');
+    const testUser = {
+      username: 'maksimpa',
+      password: '40014222',
+    };
+    try {
+      const userData = await postLogin(testUser);
+      setIsLoggedIn(true);
+      await AsyncStorage.setItem('userToken', userData.token);
+    } catch (error) {
+      console.error('postLogin error', error);
+      // TODO: add user notification about login error
     }
   };
 
   const getToken = async () => {
     const userToken = await AsyncStorage.getItem('userToken');
     console.log('token', userToken);
-    if (userToken === 'abc') {
-      setIsLoggedIn(true);
-      navigation.navigate('Home');
+    if (userToken) {
+      try {
+        await checkToken(userToken);
+        setIsLoggedIn(true);
+        navigation.navigate('Home');
+      } catch (error) {
+        console.log('token check failed', error.message);
+      }
     }
   };
   useEffect(() => {
     getToken();
+    if (isLoggedIn) {
+      // this is to make sure isLoggedIn has changed, will be removed later
+      navigation.navigate('Home');
+    }
   }, []);
 
   return (
