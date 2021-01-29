a7a136f691479a4862943d9c45dfefb11205c125
maksimpa <maksim.pasnitsenko@metropolia.fi>
2021-01-29 11:43:06 +0200

    smartforms task 2 incomplete

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -13,8 +13,10 @@ const RegisterForm = ({navigation}) => {
   const {
     inputs,
     handleInputChange,
+    handleInputEnd,
     usernameError,
     checkUserAvailable,
+    registerErrors,
   } = useSignUpForm();
   const {postRegister} = useUser();
   const {postLogin} = useLogin();
@@ -43,25 +45,38 @@ const RegisterForm = ({navigation}) => {
         onChangeText={(txt) => handleInputChange('username', txt)}
         onEndEditing={(event) => {
           // console.log(event.nativeEvent.text);
-          checkUserAvailable(event);
+          // checkUserAvailable(event);
+          handleInputEnd('username', event.nativeEvent.text);
         }}
-        errorMessage={usernameError}
+        errorMessage={registerErrors.username}
       />
       <Input
         autoCapitalize="none"
         placeholder="password"
         onChangeText={(txt) => handleInputChange('password', txt)}
+        onEndEditing={(event) =>
+          handleInputEnd('password', event.nativeEvent.text)
+        }
         secureTextEntry={true}
+        errorMessage={registerErrors.password}
       />
       <Input
         autoCapitalize="none"
         placeholder="email"
         onChangeText={(txt) => handleInputChange('email', txt)}
+        onEndEditing={(event) =>
+          handleInputEnd('email', event.nativeEvent.text)
+        }
+        errorMessage={registerErrors.email}
       />
       <Input
         autoCapitalize="none"
         placeholder="full name"
         onChangeText={(txt) => handleInputChange('full_name', txt)}
+        onEndEditing={(event) =>
+          handleInputEnd('full_name', event.nativeEvent.text)
+        }
+        errorMessage={registerErrors.full_name}
       />
       <Button title="Register!" onPress={doRegister} />
     </View>
--- hooks/RegisterHooks.js
+++ hooks/RegisterHooks.js
@@ -1,8 +1,45 @@
 import {useState} from 'react';
+import {validator} from '../utils/validator';
 import {useUser} from './ApiHooks';
+
+const constraints = {
+  username: {
+    presence: {
+      message: 'cannot be empty',
+    },
+    length: {
+      minimum: 3,
+      message: 'min length is 3 characters',
+    },
+  },
+  password: {
+    presence: {
+      message: 'cannot be empty',
+    },
+    length: {
+      minimum: 5,
+      message: 'min length is 5 characters',
+    },
+  },
+  email: {
+    presence: {
+      message: 'cannot be empty',
+    },
+    email: {
+      message: 'is not valid',
+    },
+  },
+  full_name: {
+    length: {
+      minimum: 5,
+      message: 'min length is 5 characters',
+    },
+  },
+};
 
 const useSignUpForm = (callback) => {
   const [usernameError, setUsernameError] = useState('');
+  const [registerErrors, setRegisterErrors] = useState({});
   const {checkIsUserAvailable} = useUser();
 
   const [inputs, setInputs] = useState({
@@ -23,6 +60,20 @@ const useSignUpForm = (callback) => {
     });
   };
 
+  const handleInputEnd = (name, text) => {
+    console.log('input end text', text);
+    if (text === '') {
+      text = null;
+    }
+    const error = validator(name, text, constraints);
+    setRegisterErrors((registerErrors) => {
+      return {
+        ...registerErrors,
+        [name]: error,
+      };
+    });
+  };
+
   const checkUserAvailable = async (event) => {
     // console.log('username input', event.nativeEvent.text);
     try {
@@ -39,9 +90,11 @@ const useSignUpForm = (callback) => {
 
   return {
     handleInputChange,
+    handleInputEnd,
     inputs,
     usernameError,
     checkUserAvailable,
+    registerErrors,
   };
 };
 
