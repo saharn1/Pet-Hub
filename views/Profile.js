66ef13726e88c3df7a4f25b39282d31932d0165a
ZIANG ZHAO <ZiangZhao1227@users.noreply.github.com>
2021-03-01 23:30:49 +0200

    set profile image function almost done

--- views/Profile.js
+++ views/Profile.js
@@ -7,16 +7,18 @@ import {Card, Text, ListItem, Avatar} from 'react-native-elements';
 import {useTag} from '../hooks/ApiHooks';
 import {uploadsUrl} from '../utils/Variables';
 import {ScrollView} from 'react-native-gesture-handler';
+import * as ImagePicker from 'expo-image-picker';
 
 const Profile = ({navigation}) => {
   const {isLoggedIn, setIsLoggedIn, user} = useContext(MainContext);
   const [avatar, setAvatar] = useState('http://placekitten.com/640');
+  const [filetype, setFiletype] = useState('');
   const {getFilesByTag} = useTag();
+
   const logout = async () => {
     setIsLoggedIn(false);
     await AsyncStorage.clear();
     if (!isLoggedIn) {
-      // this is to make sure isLoggedIn has changed, will be removed later
       navigation.navigate('Login');
     }
   };
@@ -24,7 +26,7 @@ const Profile = ({navigation}) => {
   useEffect(() => {
     const fetchAvatar = async () => {
       try {
-        const avatarList = await getFilesByTag('avatar_' + user.user_id);
+        const avatarList = await getFilesByTag('_avatar' + user.user_id);
         if (avatarList.length > 0) {
           setAvatar(uploadsUrl + avatarList.pop().filename);
         }
@@ -33,29 +35,85 @@ const Profile = ({navigation}) => {
       }
     };
     fetchAvatar();
+    getData();
   }, []);
+
+  const setData = async (result) => {
+    try {
+      const jsonValue = JSON.stringify(result)
+      await AsyncStorage.setItem('@storage_Key', jsonValue)
+    } catch (e) {
+      // saving error
+    }
+  }
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@storage_Key');
+      if (value !== null) {
+      }
+    } catch (e) {
+      console.log('getting data error');
+    }
+  };
+
+  const pickImage = async (library) => {
+    let result = null;
+    const options = {
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 0.5,
+    };
+    if (library) {
+      result = await ImagePicker.launchImageLibraryAsync(options);
+      setData(result);
+    } else {
+      result = await ImagePicker.launchCameraAsync(options);
+      setData(result);
+    }
+
+    console.log(result);
+
+
+    if (!result.cancelled) {
+      setFiletype(result.type);
+      setAvatar(result.uri);
+    }
+
+
+  };
 
   return (
     <ScrollView>
       <Card containerStyle={{backgroundColor: '#FFDCDC'}}>
         <Card.Title>
-          <Text h1 style={{color:'white'}}>{user.username}</Text>
+          <Text h1 style={{color: 'black'}}>
+            {user.username}
+          </Text>
         </Card.Title>
         <Card.Image
+          onPress={() => pickImage(true)}
+          buttonStyle={{backgroundColor: '#1ABBD1', size: 20, marginBottom: 20}}
           source={{uri: avatar}}
           style={styles.image}
           PlaceholderContent={<ActivityIndicator />}
         />
-        <ListItem >
+
+        <ListItem>
           <Avatar icon={{name: 'email', color: '#1ABBD1'}} />
           <Text>{user.email}</Text>
         </ListItem>
-        <ListItem>
-          <Avatar icon={{name: 'user', type: 'font-awesome', color: '#1ABBD1'}} />
+        <ListItem bottomDivider>
+          <Avatar
+            icon={{name: 'user', type: 'font-awesome', color: '#1ABBD1'}}
+          />
           <Text>{user.full_name}</Text>
         </ListItem>
-        <ListItem  onPress={() => console.log('user likes')}>
-          <Avatar icon={{name: 'crow',type:'font-awesome-5', color: '#1ABBD1'}} />
+        <ListItem bottomDivider>
+          <Avatar
+            icon={{name: 'crow', type: 'font-awesome-5', color: '#1ABBD1'}}
+          />
           <ListItem.Content>
             <ListItem.Title>My pet cart</ListItem.Title>
           </ListItem.Content>
